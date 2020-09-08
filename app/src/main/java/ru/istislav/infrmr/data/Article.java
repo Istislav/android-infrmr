cc43bd56e3bd18450075de425bf1040de396f608
CyberUnit <cyberunit@gmail.com>
2020-09-08 19:48:28 +0300

    Article model and AppController class added in the manifest as application name

---
+++ app/src/main/java/ru/istislav/infrmr/data/Article.java
@@ -0,0 +1,63 @@
+package ru.istislav.infrmr.data;
+
+// Your API key is: ae080eeb2454486b9a39f0dc5a4f9467
+
+public class Article {
+    private String author;
+    private String title;
+    private String description;
+    private String newsUrl;
+    private String imageUrl;
+    private String publishedDate;
+
+
+
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getNewsUrl() {
+        return newsUrl;
+    }
+
+    public void setNewsUrl(String newsUrl) {
+        this.newsUrl = newsUrl;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getPublishedDate() {
+        return publishedDate;
+    }
+
+    public void setPublishedDate(String publishedDate) {
+        this.publishedDate = publishedDate;
+    }
+}
