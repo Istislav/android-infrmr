cd34847b733d4d85ecf91f7c58738c8c22d6220e
CyberUnit <cyberunit@gmail.com>
2020-09-08 20:01:49 +0300

    article viewholder in the ArticleAdapter class

--- app/src/main/java/ru/istislav/infrmr/data/ArticleAdapter.java
+++ app/src/main/java/ru/istislav/infrmr/data/ArticleAdapter.java
@@ -5,6 +5,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -32,7 +34,12 @@ public class ArticleAdapter extends RecyclerView.Adapter<ArticleAdapter.ViewHold
 
     @Override
     public void onBindViewHolder(@NonNull ArticleAdapter.ViewHolder holder, int position) {
-
+        Article article = articles.get(position);
+        // this is our viewholder with class ViewHolder at the bottom
+        holder.title.setText(article.getTitle());
+        holder.description.setText(article.getDescription());
+        holder.date.setText(article.getPublishedDate());
+        holder.author.setText(article.getAuthor());
     }
 
     @Override
@@ -41,8 +48,17 @@ public class ArticleAdapter extends RecyclerView.Adapter<ArticleAdapter.ViewHold
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+        public ImageView articleImage;
+        public TextView author, description, title, date;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            articleImage = itemView.findViewById(R.id.newsImageId);
+            author = itemView.findViewById(R.id.author);
+            description = itemView.findViewById(R.id.descriptionNews);
+            title = itemView.findViewById(R.id.newsTitle);
+            date = itemView.findViewById(R.id.date);
         }
     }
 }
