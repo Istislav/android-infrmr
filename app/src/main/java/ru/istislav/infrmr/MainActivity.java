95e7f79ba682d4ff6972af79a3aeb5850ad58bf9
CyberUnit <cyberunit@gmail.com>
2020-09-11 14:13:03 +0300

    date line function, getting json on app start

--- app/src/main/java/ru/istislav/infrmr/MainActivity.java
+++ app/src/main/java/ru/istislav/infrmr/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import ru.istislav.infrmr.data.ArticleData;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new ArticleData().getNewsList();
     }
 }
--- app/src/main/java/ru/istislav/infrmr/data/ArticleData.java
+++ app/src/main/java/ru/istislav/infrmr/data/ArticleData.java
@@ -1,6 +1,7 @@
 package ru.istislav.infrmr.data;
 
 import android.app.VoiceInteractor;
+import android.util.Log;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
@@ -9,7 +10,11 @@ import com.android.volley.toolbox.JsonObjectRequest;
 
 import org.json.JSONObject;
 
+import java.text.SimpleDateFormat;
+import java.time.ZonedDateTime;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 import ru.istislav.infrmr.controller.AppController;
 
@@ -17,11 +22,12 @@ public class ArticleData {
     ArrayList<Article> articles = new ArrayList<>();
 
     public void getNewsList() {
-        String url = "http://newsapi.org/v2/everything?q=bitcoin&from=2020-08-08&sortBy=publishedAt&apiKey=ae080eeb2454486b9a39f0dc5a4f9467";
+        String url = "http://newsapi.org/v2/everything?q=bitcoin&from=" + getDateLine() + "&sortBy=publishedAt&apiKey=ae080eeb2454486b9a39f0dc5a4f9467";
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,
                 url, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
+                // Log.d("NEWSSSSSSSSSSSSS:", response.toString());
 
             }
         }, new Response.ErrorListener() {
@@ -31,6 +37,23 @@ public class ArticleData {
             }
         });
 
+        // my class AppController will write Json result to the request queue
         AppController.getInstance().addToRequestQueue(jsonObjectRequest);
     }
+
+    private String getDateLine() {
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+        Date date = null;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            // modern method (API 26)
+            date = Date.from(ZonedDateTime.now().minusDays(7).toInstant());
+        } else { // old method
+            Calendar calendar = Calendar.getInstance();
+            // calendar.add(Calendar.HOUR_OF_DAY, 24*7);
+            calendar.add(Calendar.DAY_OF_MONTH, -7);
+            date = calendar.getTime();
+        }
+        return formatter.format(date);
+    }
+
 }
